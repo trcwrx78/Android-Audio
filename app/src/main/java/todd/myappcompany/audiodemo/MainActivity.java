2c26e8c15a2275881ed3550630495d2c30d3e3d4
Torrence Cole <trcole@gmail.com>
2021-03-24 09:44:16 -0400

    worked with sliders for audio control

--- app/src/main/java/todd/myappcompany/audiodemo/MainActivity.java
+++ app/src/main/java/todd/myappcompany/audiodemo/MainActivity.java
@@ -2,21 +2,92 @@ package todd.myappcompany.audiodemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 import android.widget.VideoView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
+    AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
         mediaPlayer = (MediaPlayer) MediaPlayer.create(this, R.raw.marbles);
+
+        SeekBar volumeControl = (SeekBar) findViewById(R.id.volumeSeekBar);
+        SeekBar scrubControl = (SeekBar) findViewById(R.id.scrubSeekBar);
+
+        volumeControl.setMax(maxVolume);
+        volumeControl.setProgress(currentVolume);
+
+        scrubControl.setMax(mediaPlayer.getDuration());
+
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                Log.i("SeekBar changed", Integer.toString(i));
+
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        scrubControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                Log.i("Scrub Seek", Integer.toString(i));
+
+                mediaPlayer.seekTo(i);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+
+                scrubControl.setProgress(mediaPlayer.getCurrentPosition());
+
+            }
+        }, 0, 300);
     }
 
     public void play(View view) {
